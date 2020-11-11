693c09bfb9f11bf984d418e09004786ec75b8f3c
Hinageshi <2247001665@qq.com>
2020-11-11 18:27:46 +0800

    首次提交

---
+++ source/LinkList.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef struct node {
+	int data;
+	struct node* next;
+}node;
+
+void inItList(node*& head, node*& tail) {
+	head = new node;
+	if (!head) exit(1);
+	tail = head;
+	tail->next = NULL;
+}
+
+void tailInsert(node*& head, node*& tail, int dataIn) {
+	node* p = new node;
+	p->next = NULL;
+	p->data = dataIn;
+	tail->next = p;
+	tail = p;
+}
+
+void headInsert(node*& head, node*& tail, int dataIn) {
+	node* p = new node;
+	p->data = dataIn;
+	p->next = head->next;
+	if (head->next == NULL) tail = p;
+	head->next = p;
+}
+
+void incrementalInsert(node*& head, node*& tail, int dataIn) {
+	if (head->next == NULL) {
+		tailInsert(head, tail, dataIn);
+		return;
+	}
+	if (head->next->data >= dataIn) {
+		headInsert(head, tail, dataIn);
+		return;
+	}
+	node* p, * tmp = new node;
+	tmp->data = dataIn;
+	p = head->next;
+	while (p->next != NULL) {
+		if (p->data <= dataIn && p->next->data >= dataIn) {
+			tmp->next = p->next;
+			p->next = tmp;
+			return;
+		}
+		p = p->next;
+	}
+	tailInsert(head, tail, dataIn);
+}
+
+void deleteEven(node*& head) {
+	node* p = head, * tmp;
+	while (true)
+	{
+		if (p->next == NULL) return;
+		if (!(p->next->data & 1)) {
+			tmp = p->next;
+			p->next = p->next->next;
+			delete(tmp);
+		}
+		else p = p->next;
+	}
+}
+
+void printList(node* p) {//from head->next to tail
+	p = p->next;
+	while (true)
+	{
+		cout << p->data << " ";
+		if (p->next == NULL) return;
+		p = p->next;
+	}
+}
---
+++ source/SimpleCalculator.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+void operate(stack<int> &num, stack<char> &opr) {
+    if (num.size() > 1) {
+        int tmp = num.top() , ans;
+        num.pop();
+        if (opr.top() == '+') 
+            ans = num.top() + tmp;
+        if (opr.top() == '-') 
+            ans = num.top() - tmp;
+        if (opr.top() == '*') 
+            ans = num.top() * tmp;
+        if (opr.top() == '/') 
+            ans = num.top() / tmp;
+        num.pop();
+        num.push(ans);
+        opr.pop();
+    }
+}
+
+void calculator()
+{
+    stack<int> num;
+    stack<char> opr;
+    double tmp,ans;
+    char x;
+    while (cin >> x && x != '#') {
+        if (x >= '0' && x <= '9') 
+            num.push(x - '0');
+        else {
+            if (opr.empty()) 
+                opr.push(x);
+            else {
+                if ((x == '+' || x == '-') && (opr.top() == '*' || opr.top() == '/')) {
+                    while (!opr.empty() && opr.top() != '(') 
+                        operate(num, opr);
+                    opr.push(x);
+                }
+                else if (x != ')')
+                    opr.push(x);
+                else{
+                    while (opr.top() != '(') 
+                        operate(num, opr);
+                    opr.pop();
+                }
+            }
+        }
+    }
+    while (!opr.empty()) 
+        operate(num, opr);
+    cout << num.top();
+}
