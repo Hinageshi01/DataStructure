7aa7d2370f4131e030358d23ea2948257925dbfd
Hinageshi <2247001665@qq.com>
2020-11-21 23:49:45 +0800

    11/21

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
-int main() {
-	
-	return 0;
-}
+//int main() {
+//	int num = 0;
+//	char p, pre;
+//	pre = p=NULL;
+//	while (scanf("%c",&p) && p!='\n'){
+//		if (pre >= 'A' && pre <= 'Z' && p == '@')
+//			num++;
+//		pre = p;
+//	}
+//	std::cout << num;
+//	return 0;
+//}
